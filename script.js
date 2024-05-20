9060da50625b58051000036e2f4d172d807ce7c0
Prts11 <david.prats@cesjuanpablosegundo.es>
2024-05-20 20:05:45 +0200

    aa

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-let search=document.querySelector('.search-box');
+let search =document.querySelector('.search-box');
 
 document.querySelector('#search-icon').onclick=()=>{
     search.classList.toggle('active');
+}
+
+let menu =document.querySelector('.navbar');
+
+document.querySelector('#menu-icon').onclick=()=>{
+    menu.classList.toggle('active');
 }
 
 let header =document.querySelector('header');
